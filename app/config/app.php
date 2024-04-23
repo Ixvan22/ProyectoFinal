130e25a8abff2a91ea6b86d8de5eebd30b9dbba8
Ivan Garcia <ivangarciacarrasco22@gmail.com>
2024-04-23 11:21:41 +0200

    app config

---
+++ app/config/app.php
@@ -0,0 +1,7 @@
+<?php
+
+define("APP_URL", "http://localhost/logistica/");
+define("APP_SESSION_NAME", "logisitca");
+
+date_default_timezone_set("Europe/Madrid");
+?>
---
+++ app/config/db.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "logistica");
+define("DB_USER", "alumno");
+define("DB_PASSWORD", "alumno");
+
+?>
