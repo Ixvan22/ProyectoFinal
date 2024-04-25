fd18f464c7c12da674fa2f12b2cfa960dae34612
Iván García <ivangarciacarrasco22@gmail.com>
2024-04-25 20:59:32 +0200

    inicio estructura

--- app/config/app.php
+++ app/config/app.php
@@ -1,6 +1,6 @@
 <?php
 
-define("APP_URL", "http://localhost/logistica/");
+define("APP_URL", "http://localhost/");
 define("APP_SESSION_NAME", "logisitca");
 
 date_default_timezone_set("Europe/Madrid");
