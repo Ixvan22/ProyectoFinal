292e4f9b2aa8a0fece0b8beaf9d314af628b85a4
Ivan Garcia <ivangarciacarrasco22@gmail.com>
2024-05-06 16:35:59 +0200

    metodo andirClienteControlador creado

---
+++ app/controllers/clientecontroller.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace app\controllers;
+use app\models\mainModel;
+
+class clienteController extends mainModel {
+    public function anadirClienteControlador() {
+        $dni = $this->limpiarCadena($_POST["nuevo-cliente-dni"]);
+        $nombre = $this->limpiarCadena($_POST["nuevo-cliente-nombre"]);
+        $apellidos = $this->limpiarCadena($_POST["nuevo-cliente-apellidos"]);
+        $telefono = $this->limpiarCadena($_POST["nuevo-cliente-telefono"]);
+        $correo = $this->limpiarCadena($_POST["nuevo-cliente-correo"]);
+
+        // Verificar que no existe el usuario
+        $verificarCliente = "SELECT dni FROM usuarios WHERE dni = '$dni'";
+        $verificarCliente = $this->ejecutarConsulta($verificarCliente);
+
+        if ($verificarCliente->rowCount() == 1) {
+            $alerta = $this->alertController->alertaSimple('error', 'El cliente ya existe');
+            return $alerta;
+        }
+
+        // Verificar dni
+        $dni = mb_strtoupper($dni);
+        if (!$this->verificarDatos('[0-9]{8}[A-Z]', $dni)) {
+            $alerta = $this->alertController->alertaSimple('error', 'El dni no es válido');
+            return $alerta;
+        }
+
+        // Verificar telefono
+        if (!$this->verificarDatos('[67]{1}[0-9]{8}', $telefono)) {
+            $alerta = $this->alertController->alertaSimple('error', 'El teléfono no es válido');
+            return $alerta;
+        }
+
+        $datosCliente = [
+            [
+                "campo_nombre" => "dni",
+                "campo_marcador" => ":dni",
+                "campo_valor" => $dni
+            ],
+            [
+                "campo_nombre" => "nombre",
+                "campo_marcador" => ":nombre",
+                "campo_valor" => $nombre
+            ],
+            [
+                "campo_nombre" => "apellidos",
+                "campo_marcador" => ":apellidos",
+                "campo_valor" => $apellidos
+            ],
+            [
+                "campo_nombre" => "telefono",
+                "campo_marcador" => ":telefono",
+                "campo_valor" => $telefono
+            ],
+            [
+                "campo_nombre" => "correo",
+                "campo_marcador" => ":correo",
+                "campo_valor" => $correo
+            ]
+        ];
+
+        $anadirCliente = $this->guardarDatos("usuarios", $datosCliente);
+
+        if ($anadirCliente->rowCount() == 1) {
+            if (isset($_POST["anadir-mercancia"])) {
+                return true;
+            }
+            $alerta = $this->alertController->alertaRecargar('success', 'Cliente añadido', APP_URL.'clientes');
+            
+
+        } else {
+            if (isset($_POST["anadir-mercancia"])) {
+                return false;
+            }
+            $alerta = $this->alertController->alertaSimple('error', 'Error al añadir el cliente');
+        }
+
+        return $alerta;
+    }
+}
+
+?>
